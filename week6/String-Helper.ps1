b8abf711d23a32f14c4f30bfca92c85fe2ca6cb4
Neloyy <jsyolen@gmail.com>
2024-09-30 12:41:25 -0400

    Week 6 in class lab

---
+++ week6/String-Helper.ps1
@@ -0,0 +1,40 @@
+﻿<# String-Helper
+*************************************************************
+   This script contains functions that help with String/Match/Search
+   operations. 
+************************************************************* 
+#>
+
+
+<# ******************************************************
+   Functions: Get Matching Lines
+   Input:   1) Text with multiple lines  
+            2) Keyword
+   Output:  1) Array of lines that contain the keyword
+********************************************************* #>
+function getMatchingLines($contents, $lookline){
+
+$allines = @()
+$splitted =  $contents.split([Environment]::NewLine)
+
+for($j=0; $j -lt $splitted.Count; $j++){  
+ 
+   if($splitted[$j].Length -gt 0){  
+        if($splitted[$j] -ilike $lookline){ $allines += $splitted[$j] }
+   }
+
+}
+
+return $allines
+}
+function checkPassword($password){
+    $bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
+    $plainPass = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
+    if($plainPass.Length -lt 6){
+        return $false
+    }
+    if($plainPass -match '[^a-zA-Z0-9]'-and $plainPass -match '[0-9]' -and $plainPass -match '[a-zA-Z]'){
+        return $true
+    }
+    
+}
